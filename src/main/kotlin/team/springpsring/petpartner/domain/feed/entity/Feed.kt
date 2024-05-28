21c2db6cbc1a2e2096d6b635f25cba006e0284af
HR <dev.muun07@gmail.com>
2024-05-28 15:03:32 +0900

    Feat : Feed, Comment Entity에 toResponse 함수 생성

--- src/main/kotlin/team/springpsring/petpartner/domain/feed/entity/Feed.kt
+++ src/main/kotlin/team/springpsring/petpartner/domain/feed/entity/Feed.kt
@@ -2,40 +2,55 @@ package team.springpsring.petpartner.domain.feed.entity
 
 import jakarta.persistence.*
 import team.springpsring.petpartner.domain.feed.comment.entity.Comment
+import team.springpsring.petpartner.domain.feed.dto.FeedResponse
 import java.time.LocalDateTime
 
 @Entity
-@Table(name = "Feed")
+@Table(name = "feed")
 class Feed(
 
     @Column(name = "name", nullable = false)
     var name: String,
 
     @Column(name = "title", nullable = false)
-    var title:String,
+    var title: String,
 
     @Column(name = "body", nullable = false)
-    var body:String,
+    var body: String,
 
-    @Column(name = "imglist", nullable = false)
-    var imgList:String,
+    @Column(name = "images", nullable = false)
+    var images: String,
 
     @Column(name = "category", nullable = false)
-    var category:Int,
+    var category: Int,
 
-    @Column(name = "like", nullable = false)
-    var like:Int,
+    @Column(name = "loves", nullable = false)
+    var loves: Int=0,
 
-    @Column(name = "view", nullable = false)
-    var view:Int,
+    @Column(name = "views", nullable = false)
+    var views: Int=0,
 
     @Column(name = "created_at", nullable = false)
     var created: LocalDateTime,
 
-    @OneToMany(mappedBy="Feed",cascade = [(CascadeType.ALL)], orphanRemoval = true)
-    var comments:MutableList<Comment> = mutableListOf(),
-    ) {
+    @OneToMany(mappedBy = "feed", cascade = [(CascadeType.ALL)], orphanRemoval = true)
+    var comments: MutableList<Comment> = mutableListOf(),
+) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id:Long?=null
+    var id: Long? = null
+}
+
+fun Feed.toResponse(): FeedResponse {
+    return FeedResponse(
+        id = id!!,
+        name = name,
+        title = title,
+        body = body,
+        images = images,
+        category = category,
+        loves = loves,
+        views = views,
+        created = created,
+    )
 }
