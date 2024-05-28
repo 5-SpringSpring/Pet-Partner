f94f6e4bc01799512bd70799e5f0e62023f766a3
HR <dev.muun07@gmail.com>
2024-05-28 15:04:21 +0900

    Feat : Feed dto 작성

---
+++ src/main/kotlin/team/springpsring/petpartner/domain/feed/dto/UpdateFeedRequest.kt
@@ -0,0 +1,11 @@
+package team.springpsring.petpartner.domain.feed.dto
+
+import java.time.LocalDateTime
+
+data class UpdateFeedRequest (
+    val title: String,
+    val body:String,
+    val images : String,
+    val category:Int,
+    val created: LocalDateTime,
+)
