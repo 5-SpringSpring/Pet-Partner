bc4a5d5142bf41b1ec754227f6dad773a9f31aea
HR <dev.muun07@gmail.com>
2024-05-30 16:48:05 +0900

    Feat : Entity 구조 변경

--- src/main/kotlin/team/springpsring/petpartner/domain/feed/comment/entity/Comment.kt
+++ src/main/kotlin/team/springpsring/petpartner/domain/feed/comment/entity/Comment.kt
@@ -3,23 +3,18 @@ package team.springpsring.petpartner.domain.feed.comment.entity
 import jakarta.persistence.*
 import team.springpsring.petpartner.domain.feed.comment.dto.CommentResponse
 import team.springpsring.petpartner.domain.feed.entity.Feed
+import team.springpsring.petpartner.domain.user.entity.User
 import java.time.LocalDateTime
 
 @Entity
-@Table(name="comment")
-class Comment (
+@Table(name = "comment")
+class Comment(
 
     @Column(name = "name", nullable = false)
-    var name:String,
-
-    @Column(name = "password", nullable = false)
-    var password:String,
+    var username: String,
 
     @Column(name = "body", nullable = false)
-    var body:String,
-
-    @Column(name = "loves", nullable = false)
-    var loves :Int,
+    var body: String,
 
     @Column(name = "created_at", nullable = false)
     var createdAt: LocalDateTime,
@@ -27,19 +22,18 @@ class Comment (
     @ManyToOne
     @JoinColumn(name = "feed_id", nullable = false)
     var feed: Feed,
-){
+) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id:Long?=null
+    var id: Long? = null
 }
 
 fun Comment.toResponse()
         : CommentResponse {
     return CommentResponse(
         id = id!!,
-        name = name,
+        name = username,
         body = body,
-        loves = loves,
         createdAt = createdAt,
     )
 }
