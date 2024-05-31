af51c3aa7196bbd5dfbfe609f9a12da276364d8d
gooddle <vptl6559@gmail.com>
2024-05-31 18:00:45 +0900

    style : love 도메인 코드 정리

--- src/main/kotlin/team/springpsring/petpartner/domain/love/repository/LoveRepository.kt
+++ src/main/kotlin/team/springpsring/petpartner/domain/love/repository/LoveRepository.kt
@@ -4,7 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import team.springpsring.petpartner.domain.love.entity.Love
 
 interface LoveRepository : JpaRepository<Love, Long> {
-    fun findAllByFeedId(feedId:Long):List<Love>
-    fun findAllByLoginId(loginId:String):List<Love>
     fun findByFeedIdAndLoginId(feedId:Long, loginId:String):Love?
 }
